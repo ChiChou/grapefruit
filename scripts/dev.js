b9348791373bd56679421ce82a6a5e0363e0b71c
codecolorist <codecolorist@gmail.com>
2020-03-30 22:30:12 +0800

    Windows Terminal development env

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,62 +1,94 @@
-const cp = require('child_process')
-const path = require('path')
+const cp = require('child_process');
+const path = require('path');
+const { platform } = require('os');
 
+const isWin = platform() === 'win32'
 
 function *tasks() {
   const spec = {
     gui: ['run', 'serve'],
     agent: ['run', 'watch'],
     server: ['run', 'dev']
-  }
+  };
 
   for (let [name, args] of Object.entries(spec)) {
-    const cwd = path.join(__dirname, '..', name)
-    yield [args, cwd]
+    const cwd = path.join(__dirname, '..', name);
+    yield [args, cwd];
   }
 }
 
-function findTmux() {
+function tryRun(cmd) {
   try {
-    return cp.execSync('which tmux').toString().trim()
+    return cp.execSync(cmd).toString().trim();
   } catch(_) {
 
   }
 }
 
-const location = findTmux()
-if (location) {
-  tmux(location, tasks())
-} else {
-  run(tasks())
+function platformize(tool, args) {
+  let bin = tool;
+  let joint = args;
+  if (isWin) {
+    bin = 'cmd.exe';
+    joint = ['/c', tool, ...args];
+  }
+  return [bin, joint];
 }
 
-function run(queue) {
+function run() {
+  if (isWin) {
+    console.error('Windows platform is not supported. You should manually start 3 terminals, or install Windows Terminal from Microsoft Store');
+    console.error(`It's gonna be a mess here`);
+  }
+
   function handler() {
-    console.log('❌child process disconected')
-    process.exit()
+    console.log('❌child process disconected');
+    process.exit();
   }
 
   const children = []
-  for (let [args, cwd] of queue) {
-    const p = cp.spawn('npm', args, { stdio: 'inherit', cwd })
-    p.on('disconnect', handler)
+  for (let [args, cwd] of tasks()) {
+    const [bin, argv] = platformize('npm', args);
+    const p = cp.spawn(bin, argv, { stdio: 'inherit', cwd });
+    p.on('disconnect', handler);
   }
 
-  process.on('SIGINT', () => children.filter(p => pid).forEach(p => p.kill()))
+  process.on('SIGINT', () => children.filter(p => pid).forEach(p => p.kill()));
 }
 
-function tmux(location, queue) {
-  const argv = ['new-session']
-  for (let [args, cwd] of queue) {
-    argv.push('-c', cwd, 'npm')
-    argv.push(...args)
+function tmux() {
+  const argv = ['new-session'];
+  for (let [args, cwd] of tasks()) {
+    argv.push('-c', cwd, 'npm');
+    argv.push(...args);
     argv.push(';', 'split-window')
   }
-  // remove last split-window
-  argv.pop()
+  // last split-window
+  argv.push('yes')
   // C-a space
-  argv.push('next-layout')
+  argv.push('next-layout');
 
-  cp.spawnSync(location, argv, { stdio: 'inherit' })
+  cp.spawnSync('tmux', argv, { stdio: 'inherit' });
 }
 
+function wt() {
+  const argv = [];
+  for (let [args, cwd] of tasks()) {
+    argv.push('-d', cwd, 'cmd', '/c', 'npm')
+    argv.push(...args)
+    argv.push(';', 'new-tab')
+  }
+  argv.push('cmd', '-c', 'echo OK')
+  cp.spawn('wt', argv)
+  process.exit() // detach
+}
+
+
+if (isWin && tryRun('where wt')) {
+  wt()
+} else if (tryRun('which tmux')) {
+  tmux()  
+} else {
+  // crappy terminal
+  run(tasks())
+}
