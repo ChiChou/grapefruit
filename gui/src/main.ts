6ea641bf7146aa23fe491069561e102b5b841cc1
codecolorist <codecolorist@gmail.com>
2020-04-15 20:33:23 +0800

    add golden-layout

--- gui/src/main.ts
+++ gui/src/main.ts
@@ -6,6 +6,9 @@ import 'buefy/dist/buefy.css'
 import '../bulmaswatch/darkly/bulmaswatch.scss'
 import '@mdi/font/css/materialdesignicons.css'
 
+import vgl from 'vue-golden-layout'
+import 'golden-layout/src/css/goldenlayout-dark-theme.css'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -13,6 +16,7 @@ import store from './store'
 Vue.config.productionTip = false
 Vue.component('split-pane', pane)
 Vue.use(Buefy)
+Vue.use(vgl)
 
 new Vue({
   router,
