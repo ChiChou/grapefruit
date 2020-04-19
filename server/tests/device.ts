2ec13aa504c7f19d6780b7d0750bdc927434a33d
codecolorist <codecolorist@gmail.com>
2020-04-19 21:04:19 +0800

    refactor: move tryGetDevice to a module

--- server/tests/device.ts
+++ server/tests/device.ts
@@ -14,7 +14,7 @@ describe('device management', () => {
     expect(apps.length).to.be.greaterThan(0)
     expect(apps[0]).to.have.keys('pid', 'identifier', 'largeIcon', 'name', 'smallIcon')
     expect(await device.open('com.apple.mobilesafari', 'about:blank')).to.gt(0)
-    expect(device.valueOf()).to.have.keys('name', 'id', 'icon', 'type')
+    expect(device.valueOf()).to.have.keys('name', 'id', 'icon', 'removable', 'type')
     expect(device.host).to.be.null
 
     const session = await device.launch('com.apple.calculator')
