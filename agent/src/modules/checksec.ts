2ad1839a7b0575405e58469baa2f218eb576a1c4
codecolorist <codecolorist@gmail.com>
2020-04-20 19:47:51 +0800

    fix endianess

--- agent/src/modules/checksec.ts
+++ agent/src/modules/checksec.ts
@@ -15,11 +15,17 @@ export default function checksec() {
   }
 
   const CS_OPS_ENTITLEMENTS_BLOB = 7
-  const csops = new NativeFunction(
+  const csops = new SystemFunction(
     Module.findExportByName('libsystem_kernel.dylib', 'csops')!,
     'int',
     ['int', 'int', 'pointer', 'uint64']
   )
+
+  // todo: determine CPU endianness
+  const ntohl = (val: number) => ((val & 0xFF) << 24)
+    | ((val & 0xFF00) << 8)
+    | ((val >> 8) & 0xFF00)
+    | ((val >> 24) & 0xFF);
 
   // struct csheader {
   //   uint32_t magic;
@@ -27,11 +33,13 @@ export default function checksec() {
   // };
 
   const SIZE_OF_CSHEADER = 8
+  const ERANGE = 34
   const csheader = Memory.alloc(SIZE_OF_CSHEADER)
-  if (csops(Process.id, CS_OPS_ENTITLEMENTS_BLOB, csheader, SIZE_OF_CSHEADER) === -1) {
-    const length = csheader.add(4).readU32()
+  const { value, errno } = csops(Process.id, CS_OPS_ENTITLEMENTS_BLOB, csheader, SIZE_OF_CSHEADER) as UnixSystemFunctionResult
+  if (value === -1 && errno === ERANGE) {
+    const length = ntohl(csheader.add(4).readU32())
     const content = Memory.alloc(length)
-    if (csops(Process.id, CS_OPS_ENTITLEMENTS_BLOB, content, length) === 0) {
+    if (csops(Process.id, CS_OPS_ENTITLEMENTS_BLOB, content, length).value === 0) {
       result.entitlements = dictFromPlistCharArray(
         content.add(SIZE_OF_CSHEADER), length - SIZE_OF_CSHEADER
       )
