5e4c9399f9f7c378bcca63b5a181414d2bed6814
codecolorist <codecolorist@gmail.com>
2020-04-20 21:16:16 +0800

    screenshot as a component

---
+++ gui/src/components/ScreenShot.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="screenshot">
+    <a v-if="device" :href="url" target="_blank" class="frame">
+      <div class="placeholder" v-if="loading">
+        <Loading v-if="loading" class="loading" />
+      </div>
+      <img :src="url" width="320" @error="onerror" @load="loading = false" :class="{ loading }">
+    </a>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Watch } from 'vue-property-decorator'
+
+import Loading from '../components/Loading.vue'
+
+@Component({
+  components: {
+    Loading
+  }
+})
+export default class Device extends Vue {
+  loading = false
+  fail = false
+  url?: string
+
+  @Prop()
+  device?: string;
+
+  @Watch('device')
+  refresh() {
+    this.loading = true
+    this.fail = false
+    this.url = `/api/device/${this.device}/screen?t=` + Math.random()
+  }
+
+  mounted() {
+    this.refresh()
+  }
+
+  onerror() {
+    this.loading = false
+    this.fail = true
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.screenshot {
+  .placeholder {
+    position: relative;
+    min-height: 620px;
+    background: #000;
+
+    > .loading {
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      margin-top: -40px;
+      margin-left: -40px;
+    }
+  }
+
+  a {
+    display: block;
+    margin-bottom: 20px;
+
+    img, .placeholder {
+      border: 10px solid #000000ad;
+      border-radius: 20px;
+
+      @media (max-width: 1800px) {
+        width: 240px;
+      }
+
+      @media (min-width: 1801px) {
+        width: 320px;
+      }
+    }
+
+    img.loading {
+      visibility: hidden;
+    }
+  }
+}
+</style>
