0525514bc7c102bf8718e842d5acb0aba799bac0
CodeColorist <codecolorist@gmail.com>
2020-04-21 22:25:06 +0800

    TextView and object viewer control

---
+++ gui/src/views/tabs/TextViewer.vue
@@ -0,0 +1,19 @@
+<template>
+  <pre>{{ text }}</pre>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component
+export default class TextViewer extends Vue {
+  @Prop()
+  text?: string
+}
+</script>
+
+<style scoped>
+pre {
+  height: 100%;
+}
+</style>
