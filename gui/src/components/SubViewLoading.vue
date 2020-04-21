fb64a9feb01ae21a3a724a6b3ce54a4800ab6ca8
codecolorist <codecolorist@gmail.com>
2020-04-21 22:25:06 +0800

    TextView and object viewer control

---
+++ gui/src/components/SubViewLoading.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="spin">
+    <loading />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import Loading from './Loading.vue'
+
+@Component({
+  components: {
+    Loading
+  }
+})
+export default class Workspace extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.spin {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+
+  .loading {
+    margin: auto;
+  }
+}
+</style>
---
+++ gui/src/views/tabs/Frame.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="subview-container">
+    <component v-if="component" :is="component" v-bind="data" />
+  </div>
+</template>
+
+<script lang="ts">
+import TextViewer from './TextViewer.vue'
+import Info from './Info.vue'
+
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component({
+  components: {
+    TextViewer,
+    Info
+  }
+})
+export default class Workspace extends Vue {
+  @Prop({ default: () => { return {} } })
+  data?: object
+
+  @Prop()
+  component?: string
+}
+</script>
+
+<style>
+.subview-container {
+  overflow-y: scroll;
+  height: 100%;
+}
+</style>
