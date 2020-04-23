8519acee4cbfccace7ca6cb98dc7482bda734592
codecolorist <codecolorist@gmail.com>
2020-04-23 23:58:58 +0800

    refactor wsapi proxy agent console to frontend handle disconnection

--- gui/src/wsrpc.d.ts
+++ gui/src/wsrpc.d.ts
@@ -1,15 +1,37 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 
 export type RPC = {
   [key: string]: RPC;
   (...args: any): any;
 }
 
+type WSEvent = 'ready' | 'destroyed' | 'exception' | 'detached' | 'console'
+
+interface Options {
+  router: VueRouter;
+}
+
+interface Context {
+  socket?: SocketIOClient.Socket;
+}
+
+interface RpcResponse {
+  status: 'ok' | 'error';
+  data: any;
+  error: string;
+}
+
+interface WS {
+  ready(): Promise<boolean>;
+  on(event: WSEvent, cb: Function): WS;
+  send(event: string, ...args: any[]): Promise<any>;
+}
+
 declare module 'vue/types/vue' {
   interface Vue {
     $rpc: RPC;
-    rpcReady(): Promise<boolean>;
-    ws(event: string, ...args: any): Promise<any>;
+    $ws: WS;
   }
 }
 
