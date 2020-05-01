b03ee605e0364d9dfff8d486958a01866b9016bd
CodeColorist <codecolorist@gmail.com>
2020-05-02 01:47:28 +0800

    file explorer todo: open file

--- gui/src/views/panels/Files.vue
+++ gui/src/views/panels/Files.vue
@@ -1,13 +1,75 @@
 <template>
-  <aside class="menu">
-    <p class="menu-label">Files</p>
-    <ul class="menu-list">
-      <li>Home</li>
-      <li>Bundle</li>
-    </ul>
+  <aside class="explorer">
+    <header>
+      <b-tabs v-model="index" expanded class="header-only sticky">
+        <b-tab-item label="Home" icon="folder-home-outline" />
+        <b-tab-item label="Bundle" icon="folder-cog-outline" />
+        <!-- <b-tab-item label="tmp" icon="timer-outline" /> -->
+      </b-tabs>
+    </header>
+
+    <main>
+      <FileTree :root="root" cwd="/" :depth="0" :item="{ type: 'directory', name: root }" />
+    </main>
   </aside>
 </template>
 
-<script>
-export default {}
+<script lang="ts">
+import { Prop, Component, Watch, Vue } from 'vue-property-decorator'
+import FileTree from '../../components/FileTree.vue'
+
+interface Context {
+  tmp?: string;
+  bundle?: string;
+  home?: string;
+}
+
+@Component({
+  components: {
+    FileTree
+  }
+})
+export default class ClassInfo extends Vue {
+  ctx: Context = {}
+  index = 0
+  loading = false
+
+  get root() {
+    return ['home', 'bundle'][this.index]
+  }
+}
 </script>
+
+<style lang="scss" scoped>
+h2 {
+  padding: 10px 20px;
+  background: #1b1b1b;
+  color: #999;
+}
+
+aside.explorer {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+
+  > header {
+
+  }
+
+  > main {
+    flex: 1;
+    overflow: auto;
+    padding: 0 10px 10px 10px;
+  }
+}
+
+.b-tabs.is-fullwidth {
+  .tabs {
+    margin-bottom: 0 !important;
+  }
+  .tab-content {
+    display: none !important;
+  }
+}
+
+</style>
