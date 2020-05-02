78f361856e9195c37d7a42afed222f3b428d48f3
codecolorist <codecolorist@gmail.com>
2020-05-02 22:51:03 +0800

    various previews

---
+++ gui/src/views/tabs/ImagePreview.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="stage">
+      <img v-if="url" :src="url">
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component } from 'vue-property-decorator'
+import InlinePreview from './InlinePreview.vue'
+
+@Component
+export default class ImagePreview extends InlinePreview {}
+</script>
+
+<style lang="scss" scoped>
+.stage {
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ gui/src/views/tabs/PDFPreview.vue
@@ -0,0 +1,11 @@
+<template>
+  <embed v-if="url" :src="url" type="application/pdf">
+</template>
+
+<script lang="ts">
+import { Component } from 'vue-property-decorator'
+import InlinePreview from './InlinePreview.vue'
+
+@Component
+export default class PDFPreview extends InlinePreview {}
+</script>
