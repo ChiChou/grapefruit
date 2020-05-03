ec3ea345f1a3b998fc560f15cd6ad3f1c52e4ab0
codecolorist <codecolorist@gmail.com>
2020-05-03 17:54:39 +0800

    make class names and functions clickable

--- gui/src/views/tabs/ModuleInfo.vue
+++ gui/src/views/tabs/ModuleInfo.vue
@@ -7,8 +7,7 @@
         <b-table
           :paginated="true"
           per-page="100"
-          :data="exports"
-          :columns="colsForExports">
+          :data="exports">
           <b-input
             v-if="!props.column.numeric"
             slot="searchable"
@@ -17,6 +16,18 @@
             placeholder="Search..."
             icon="magnify"
             size="is-small" />
+
+          <template slot-scope="props">
+            <b-table-column field="type" label="Type" sortable>
+              {{ props.row.type }}
+            </b-table-column>
+            <b-table-column field="name" label="Name" sortable searchable>
+              <a @click="disasm(props.row)">{{ props.row.demangled || props.row.name }}</a>
+            </b-table-column>
+            <b-table-column field="address" label="Address" sortable numeric>
+              <code>{{ props.row.address }}</code>
+            </b-table-column>
+          </template>
         </b-table>
       </b-tab-item>
 
@@ -25,8 +36,7 @@
         <b-table
           :paginated="true"
           per-page="100"
-          :data="imports"
-          :columns="colsForImports">
+          :data="imports">
           <b-input
             v-if="!props.column.numeric"
             slot="searchable"
@@ -35,7 +45,27 @@
             placeholder="Search..."
             icon="magnify"
             size="is-small" />
+
+          <template slot-scope="props">
+            <b-table-column field="type" label="Type" sortable>
+              {{ props.row.type }}
+            </b-table-column>
+            <b-table-column field="name" label="Name" sortable searchable>
+              <a @click="disasm(props.row)">{{ props.row.demangled || props.row.name }}</a>
+            </b-table-column>
+            <b-table-column field="address" label="Address" sortable numeric>
+              <code>{{ props.row.address }}</code>
+            </b-table-column>
+            <b-table-column field="module" label="Module" sortable searchable>
+              <a>{{ props.row.module }}</a>
+            </b-table-column>
+            <b-table-column field="slot" label="Slot" sortable>
+              <code>{{ props.row.slot }}</code>
+            </b-table-column>
+          </template>
+
         </b-table>
+
       </b-tab-item>
     </b-tabs>
   </div>
@@ -45,57 +75,19 @@
 // eslint-disable-next-line
 /// <reference path="../../frida.shim.d.ts" />
 
-import { Component, Vue, Watch, Prop } from 'vue-property-decorator'
+import { Component, Prop } from 'vue-property-decorator'
 import Base from './Base.vue'
 
-const basicColumns = [{
-  field: 'type',
-  label: 'Type',
-  width: '80',
-  sortable: true,
-  numeric: false,
-  searchable: false
-}, {
-  field: 'name',
-  label: 'Name',
-  width: '320',
-  sortable: true,
-  numeric: false,
-  searchable: true
-}, {
-  field: 'address',
-  label: 'Address',
-  width: '80',
-  sortable: true,
-  numeric: true,
-  searchable: false
-}]
-
-const importsColumns = basicColumns.slice()
-
-importsColumns.push({
-  field: 'module',
-  label: 'Module',
-  width: '640',
-  sortable: true,
-  numeric: false,
-  searchable: true
-}, {
-  field: 'slot',
-  label: 'Slot',
-  width: '80',
-  sortable: true,
-  numeric: true,
-  searchable: false
-})
+interface NativeSymbol {
+  type: string;
+  address: string;
+  name: string;
+}
 
 @Component
 export default class ModuleInfo extends Base {
   @Prop({ required: true })
   module!: Module
-
-  colsForExports = basicColumns
-  colsForImports = importsColumns
 
   imports: object[] = []
   exports: object[] = []
@@ -108,6 +100,17 @@ export default class ModuleInfo extends Base {
   async load() {
     this.imports = await this.$rpc.symbol.imps(this.module.name)
     this.exports = await this.$rpc.symbol.exps(this.module.name)
+  }
+
+  disasm(item: NativeSymbol) {
+    const classPrefix = 'OBJC_CLASS_$_'
+    if (item.type === 'variable' && item.name.startsWith(classPrefix)) {
+      const name = item.name.substring(classPrefix.length)
+      this.$bus.$emit('openTab', 'ClassInfo', 'Class: ' + name, { name })
+    } else if (item.type === 'function') {
+      const addr = item.address
+      this.$bus.$emit('openTab', 'Disasm', 'Disasm @' + addr, { addr })
+    }
   }
 }
 </script>
