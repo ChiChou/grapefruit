463fea2ae9b2aa9118ee3e1c119a7fe2b31b021b
codecolorist <codecolorist@gmail.com>
2020-05-05 15:03:45 +0800

    bugfix: ObjC.schedule could cause an UAF!!

--- agent/src/modules/url.ts
+++ agent/src/modules/url.ts
@@ -1,5 +1,4 @@
 export function open(urlStr: string) {
-	const url = ObjC.classes.NSURL.URLWithString_(urlStr)
 	const app = ObjC.classes.UIApplication.sharedApplication()
 
 	// iOS 13 UISceneDelegate
@@ -9,9 +8,11 @@ export function open(urlStr: string) {
 			const scene = scenes.objectAtIndex_(i)
 			const delegate = scene.delegate()
 			if (!delegate) continue
-			const opt = ObjC.classes.UISceneOpenURLOptions.new()
-			const ctx = ObjC.classes.UIOpenURLContext.new().initWithURL_options_(url, opt)
 			ObjC.schedule(ObjC.mainQueue, () => {
+				// prevent UAF!!
+				const url = ObjC.classes.NSURL.URLWithString_(urlStr)
+				const opt = ObjC.classes.UISceneOpenURLOptions.new()
+				const ctx = ObjC.classes.UIOpenURLContext.new().initWithURL_options_(url, opt)
 				scene.delegate().scene_openURLContexts_(scene, ObjC.classes.NSSet.setWithObject_(ctx))
 			})
 			return
@@ -29,9 +30,10 @@ export function open(urlStr: string) {
 	for (let sel of candidates) {
 		const method = delegate[sel]
 		if (typeof method === 'function') {
-			const rest = [...new Array(method.length - 2)].map(e => NULL)
 			console.log('url handler:', delegate, sel, '@' + method.implementation)
 			ObjC.schedule(ObjC.mainQueue, () => {
+				const rest = [...new Array(method.length - 2)].map(e => NULL)
+				const url = ObjC.classes.NSURL.URLWithString_(urlStr)
 				delegate[sel](app, url, ...rest)
 			})
 			return
