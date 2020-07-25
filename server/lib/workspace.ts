e28f997aab99bc2efb4539c70bdb8106d8105d22
codecolorist <codecolorist@gmail.com>
2020-07-25 21:50:58 +0800

    add typeorm models setup database

--- server/lib/workspace.ts
+++ server/lib/workspace.ts
@@ -1,13 +1,29 @@
-import path from 'path'
-import os from 'os'
+import * as path from 'path'
+import * as os from 'os'
+import { promises as fsp, Stats } from 'fs'
 
 
 export function home(): string {
+  const name = 'grapefruit'
+
   if (process.platform === 'darwin')
-    return path.join(os.homedir(), 'Library', 'Application Support', 'grapefruit')
+    return path.join(os.homedir(), 'Library', 'Application Support', name)
 
   if (process.platform === 'win32')
-    return path.join(process.env.APPDATA, 'grapefruit')
+    return path.join(process.env.APPDATA, name)
 
-  return path.join(os.homedir(), '.local', 'share', 'grapefruit')
+  return path.join(os.homedir(), '.local', 'share', name)
+}
+
+export function concat(...args: string[]): string {
+  return path.join(home(), ...args)
+}
+
+export async function setup(): Promise<void> {
+  const cwd = home()
+  try {
+    await fsp.access(cwd)
+  } catch(e) {
+    await fsp.mkdir(cwd, { recursive: true })
+  }
 }
