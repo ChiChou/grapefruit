64f99fdd2d0a30618c19c32e8e79a309071c55ed
codecolorist <codecolorist@gmail.com>
2020-11-08 00:10:42 +0800

    fix #17

--- agent/src/ready.ts
+++ agent/src/ready.ts
@@ -2,9 +2,10 @@ function requireMinimalVersion(requirement: string) {
   const parse = (ver: string) => ver.split('.').map(s => parseInt(s, 10))
   const a = parse(Frida.version), b = parse(requirement)
   for (let i = 0; i < Math.max(a.length, b.length); i++) {
-    if (a[i] < b[i]) {
+    if (a[i] < b[i])
       throw new Error(`Fatal error: requiring mininal frida version ${requirement}, found ${Frida.version}`)
-    }
+    if (a[i] > b[i])
+      return
   }
 }
 
