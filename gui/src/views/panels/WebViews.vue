05d9af51a3d1a650246e2f139961d16c6fcafb4c
CodeColorist <codecolorist@gmail.com>
2021-01-06 20:52:51 +0800

    remove unused import

--- gui/src/views/panels/WebViews.vue
+++ gui/src/views/panels/WebViews.vue
@@ -26,7 +26,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue, Watch } from 'vue-property-decorator'
+import { Component, Vue } from 'vue-property-decorator'
 
 @Component
 export default class WebViews extends Vue {
