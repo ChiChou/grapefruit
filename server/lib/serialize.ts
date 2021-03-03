a2611ee263964a9cb84efe9a460c0c9c21a1cb77
codecolorist <codecolorist@gmail.com>
2021-03-03 18:37:26 +0800

    a workaround for #20

--- server/lib/serialize.ts
+++ server/lib/serialize.ts
@@ -1,6 +1,15 @@
 import { Icon, Device, Application } from 'frida'
 import { DeviceType } from 'frida/dist/device'
 
+const major = (() => {
+  // a workaround for
+  // https://github.com/ChiChou/Grapefruit/issues/20
+  // https://github.com/frida/frida-node/issues/61
+
+  const [major, ] = process.versions.node.split('.')
+  return parseInt(major)
+})()
+
 export function icon(icon?: Icon): object {
   if (!icon) return
   const { pixels, height, width, rowstride } = icon
@@ -10,7 +19,7 @@ export function icon(icon?: Icon): object {
 export function device(dev: Device): object {
   const { name, id, type } = dev
   const removable = dev.type === DeviceType.Remote && dev.name !== 'Local Socket'
-  return { name, id, icon: icon(dev.icon), type, removable }
+  return { name, id, icon: major > 12 ? undefined : icon(dev.icon), type, removable }
 }
 
 export function app(app: Application): object {
