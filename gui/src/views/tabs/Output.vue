6530a01ac7ac7c235053f8d6a382e455480a9bb7
cc <codecolorist@gmail.com>
2021-10-23 17:30:46 +0200

    add Output tab

---
+++ gui/src/views/tabs/Output.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Watch } from 'vue-property-decorator'
+
+export default class Workspace extends Vue {
+  mounted() {
+
+  }
+}
+</script>
