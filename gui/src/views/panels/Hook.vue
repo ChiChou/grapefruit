c17b1902e725496269d968462d60d17dd144732a
cc <codecolorist@gmail.com>
2021-10-28 23:07:58 +0200

    hide hook button

--- gui/src/views/panels/Hook.vue
+++ gui/src/views/panels/Hook.vue
@@ -1,3 +1,26 @@
 <template>
-  <h1 style="padding: 10px">Todo: Hook management</h1>
+  <div>
+
+  </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue, Watch } from 'vue-property-decorator'
+
+export default class Workspace extends Vue {
+
+  mounted() {
+    this.$bus.$on('addHook', this.onAddHook)
+    this.$bus.$on('removeHook', this.onRemoveHook)
+  }
+
+  onRemoveHook() {
+    // todo:
+  }
+
+  onAddHook() {
+    // todo:
+  }
+}
+
+</script>
