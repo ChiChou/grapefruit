0a49db553f0af90dee1ffcfa8b64b06cc00e59eb
cc <codecolorist@gmail.com>
2021-12-06 01:09:07 +0100

    add shortcut to folders

--- gui/src/views/tabs/Info.vue
+++ gui/src/views/tabs/Info.vue
@@ -5,40 +5,59 @@
         <div class="media">
           <div class="media-left">
             <figure class="image is-64x64">
-              <img v-if="icon" :src="icon" alt="AppIcon">
+              <img v-if="icon" :src="icon" alt="AppIcon" />
             </figure>
           </div>
           <div class="media-content">
             <p class="title is-4">{{ info.name }}</p>
-            <p class="subtitle is-6">{{ info.id }} <span class="has-text-darker">{{ info.semVer }}</span></p>
+            <p class="subtitle is-6">
+              {{ info.id }}
+              <span class="has-text-darker">{{ info.semVer }}</span>
+            </p>
           </div>
         </div>
       </div>
     </div>
 
     <dl @click.capture="onSelectText">
-      <dt>Container</dt><dd>{{ info.home }}</dd>
-      <dt>Temporary Directory</dt><dd>{{ info.tmp }}</dd>
-      <dt>Installation</dt><dd>{{ info.bundle }}</dd>
-      <dt>Executable</dt><dd>{{ info.binary }}</dd>
-      <dt>Minimum OS Requirement</dt><dd>{{ info.minOS }}</dd>
+      <dt>
+        Container
+        <a @click="open('home')"><b-icon icon="open-in-new" /></a>
+      </dt>
+      <dd>{{ info.home }}</dd>
+      <dt>Temporary Directory</dt>
+      <dd>{{ info.tmp }}</dd>
+      <dt>Installation
+        <a @click="open('bundle')"><b-icon icon="open-in-new" /></a>
+      </dt>
+      <dd>{{ info.bundle }}</dd>
+      <dt>Executable</dt>
+      <dd>{{ info.binary }}</dd>
+      <dt>Minimum OS Requirement</dt>
+      <dd>{{ info.minOS }}</dd>
       <dt>Info.plist</dt>
       <dd>
-        <data-field class="plist dark" :depth="0" :field="{ value: info.json }" />
+        <data-field
+          class="plist dark"
+          :depth="0"
+          :field="{ value: info.json }"
+        />
       </dd>
     </dl>
   </div>
 </template>
 
 <script lang="ts">
+import router from '@/router'
+import { FinderModule } from '@/store/modules/finder'
 import { Component } from 'vue-property-decorator'
 import DataField from '../../components/DataField.vue'
 import Base from './Base.vue'
 
 @Component({
   components: {
-    DataField
-  }
+    DataField,
+  },
 })
 export default class Info extends Base {
   info: object = {}
@@ -46,12 +65,27 @@ export default class Info extends Base {
 
   mounted() {
     this.loading = true
-    this.load().finally(() => { this.loading = false })
+    this.load().finally(() => {
+      this.loading = false
+    })
+  }
+
+  open(dest: string) {
+    router.push({ name: 'Files' }).catch((_) => {})
+
+    if (dest === 'home') {
+      FinderModule.goHome()
+    } else if (dest === 'bundle') {
+      FinderModule.goApp()
+    } else {
+      throw new Error('invalid destination: ' + dest)
+    }
+    this.$bus.$emit('switchTab', 'Finder')
   }
 
   async load() {
     this.info = await this.$rpc.info.info()
-    const data = await this.$rpc.info.icon() as ArrayBuffer
+    const data = (await this.$rpc.info.icon()) as ArrayBuffer
     if (!data.byteLength) return // todo: placeholder
     const blob = new Blob([data], { type: 'image/png' })
     this.icon = URL.createObjectURL(blob)
