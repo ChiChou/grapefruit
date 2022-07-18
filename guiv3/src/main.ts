143d285a653289a30b666446d7425be95e4b8623
cc <codecolorist@gmail.com>
2022-07-19 00:56:07 +0200

    bugfix: remove unused piniajs

--- guiv3/src/main.ts
+++ guiv3/src/main.ts
@@ -3,6 +3,5 @@ import App from './App.vue'
 import router from './router'
 
 import naive from 'naive-ui'
-import { createPinia } from 'pinia'
 
-createApp(App).use(createPinia()).use(naive).use(router).mount('#app');
+createApp(App).use(naive).use(router).mount('#app');
