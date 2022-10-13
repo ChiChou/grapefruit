06bd829cd735090a8ef3608d9b3e1ee14affe575
cc <codecolorist@gmail.com>
2022-10-13 13:07:01 +0200

    workaround for dots in url
    https://github.com/vitejs/vite/issues/2415

--- guiv3/vite.config.ts
+++ guiv3/vite.config.ts
@@ -1,10 +1,11 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import pluginRewriteAll from 'vite-plugin-rewrite-all'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), pluginRewriteAll()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
