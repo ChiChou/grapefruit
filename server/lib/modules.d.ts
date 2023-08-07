c63158e58e7ff3056914d3e4e945bf910535ced5
cc <codecolorist@gmail.com>
2023-08-08 00:14:57 +0200

    remove unused plist libs

--- server/lib/modules.d.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
-declare module 'bplist-creator' {
-  export default function(obj: any): Buffer;
-}
-
-declare module 'bplist-parser' {
-  export function parseBuffer(data: Buffer): any;
-}
