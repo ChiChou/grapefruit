f219d53a5f1fd9b5208fa9f364dd9941cb6cc17b
cc <codecolorist@gmail.com>
2023-08-16 02:02:30 +0200

    display modules

--- guiv3/src/views/panels/ModulesTab.vue
+++ guiv3/src/views/panels/ModulesTab.vue
@@ -1,1 +1,50 @@
-<template>Modules</template>
+<template>
+  <n-data-table size="small" :columns="columns" :data="data" :max-height="height" virtual-scroll />
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, Ref, inject, h } from 'vue'
+import { RPC, SIDE_PANEL_HEIGHT } from '@/types'
+import { DataTableColumns } from 'naive-ui/lib/data-table';
+
+interface Module {
+  name: string;
+  base: string;
+  size: number;
+  path: string;
+}
+
+const rpc = inject(RPC)!
+
+const columns: DataTableColumns<Module> = [
+  {
+    title: 'Modules',
+    key: 'name',
+    fixed: 'left',
+    render(row) {
+      return h('code', [/*row.base, ' ', */row.name])
+    }
+  }
+]
+
+const height = inject(SIDE_PANEL_HEIGHT)!
+const data = ref<Module[]>([])
+const loading = ref(false)
+
+onMounted(async () => {
+  loading.value = true
+  data.value = await rpc.symbol.modules()
+  loading.value = false
+})
+
+</script>
+
+<style lang="scss">
+.modules-container {
+  height: 100%;
+}
+.modules-container code {
+  word-wrap: break-word;
+  white-space: pre-wrap;
+}
+</style>
