24fb54b979a29173883578e8a6f445729521ba6b
cc <codecolorist@gmail.com>
2023-08-20 22:00:23 +0200

    use Objective C uuid generator

--- agent/src/lib/uuid.ts
+++ agent/src/lib/uuid.ts
@@ -1,8 +1,3 @@
 export default function uuid(): string {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-    const r = Math.random() * 16 | 0, v = c === 'x' ? r : ((r & 0x3) | 0x8)
-    return v.toString(16)
-  })
+  return ObjC.classes.NSUUID.UUID().UUIDString().toString()
 }
-
-// return ObjC.classes.NSUUID.UUID().UUIDString().toString()
