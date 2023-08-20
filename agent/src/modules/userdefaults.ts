b972a9ab5f2711b53d42385d4e6c93d96aa076c6
cc <codecolorist@gmail.com>
2023-08-20 20:20:29 +0200

    update and delete keys from UserDefaults

--- agent/src/modules/userdefaults.ts
+++ agent/src/modules/userdefaults.ts
@@ -1,13 +1,5 @@
 import { valueOf } from '../lib/dict.js'
-import { NSDictionary, NSObject } from '../objc-types.js'
-
-interface NSUserDefaults extends NSObject {
-  dictionaryRepresentation(): NSDictionary<string, NSObject>
-}
-
-interface UserDefaultsDict {
-  [key: string]: UserDefaultsEntry
-}
+import { NSDictionary, NSArray, NSObject, NSNumber, NSString, NSDate, NSData } from '../objc-types.js'
 
 /**
  * A default object must be a property list—that is, an instance of (or for collections, 
@@ -18,6 +10,8 @@ interface UserDefaultsDict {
  */
 
 type UserDefaultsType = 'data' | 'string' | 'number' | 'date' | 'array' | 'dict'
+type UserDefaultsValue = NSString | NSData | NSNumber | NSDate |
+  NSArray<UserDefaultsValue> | NSDictionary<NSString, UserDefaultsValue>
 
 interface UserDefaultsEntry {
   type: UserDefaultsType
@@ -25,38 +19,46 @@ interface UserDefaultsEntry {
   value: any
 }
 
+interface UserDefaultsDict {
+  [key: string]: UserDefaultsEntry
+}
+
+interface NSUserDefaults extends NSObject {
+  dictionaryRepresentation(): NSDictionary<string, UserDefaultsValue>
+  objectForKey_(key: string): UserDefaultsValue
+}
+
 function typeName(value: NSObject): UserDefaultsType {
-  if (value.isKindOfClass_(ObjC.classes.NSData))
-    return 'data'
+  const mapping: {[className: string]: UserDefaultsType} = {
+    'NSData': 'data',
+    'NSString': 'string',
+    'NSNumber': 'number',
+    'NSDate': 'date',
+    'NSArray': 'array',
+    'NSDictionary': 'dict'
+  }
 
-  if (value.isKindOfClass_(ObjC.classes.NSString))
-    return 'string'
-
-  if (value.isKindOfClass_(ObjC.classes.NSNumber))
-    return 'number'
-
-  if (value.isKindOfClass_(ObjC.classes.NSDate))
-    return 'date'
-
-  if (value.isKindOfClass_(ObjC.classes.NSArray))
-    return 'array'
-
-  if (value.isKindOfClass_(ObjC.classes.NSDictionary))
-    return 'dict'
+  for (const [className, type] of Object.entries(mapping)) {
+    if (value.isKindOfClass_(ObjC.classes[className]))
+      return type
+  }
 
   throw new Error(`Unknown type: ${value}`)
 }
 
-export default function () {
-  const defaults = ObjC.classes.NSUserDefaults.standardUserDefaults() as NSUserDefaults
-  const asDict = defaults.dictionaryRepresentation()
+function singleton() {
+  return ObjC.classes.NSUserDefaults.standardUserDefaults() as NSUserDefaults
+}
+
+export function enumerate() {
+  const asDict = singleton().dictionaryRepresentation()
   const keys = asDict.allKeys()
 
-  function *gen() {
+  function* gen() {
     for (let i = 0; i < keys.count(); i++) {
       const key = keys.objectAtIndex_(i)
       const value = asDict.objectForKey_(key)
-      
+
       yield [key.toString(), {
         type: typeName(value),
         readable: value.toString(),
@@ -67,3 +69,35 @@ export default function () {
 
   return Object.fromEntries(gen()) as UserDefaultsDict
 }
+
+/*
+ * It's impossible for us to distinguish among different types of NSNumbers and boolean values.
+ * Also, we can't handle nested array and dictionary.
+ *
+ * So only support NSString, NSData and NSDate here.
+ */
+
+export function update(key: string, value: string | number) {
+  const userDefaults = singleton()
+  const current = userDefaults.objectForKey_(key)
+  if (!current) throw new Error(`key ${key} not found`)
+  if (current.isKindOfClass_(ObjC.classes.NSString) && typeof value === 'string') {
+    // string literal
+    userDefaults.setObject_forKey_(value, key)
+  } else if (current.isKindOfClass_(ObjC.classes.NSDate) && typeof value === 'number') {
+    // timestamp
+    userDefaults.setObject_forKey_(ObjC.classes.NSDate.dateWithTimeIntervalSince1970_(value), key)
+  } else if (current.isKindOfClass_(ObjC.classes.NSData) && typeof value === 'string') {
+    // base64 encoded string
+    userDefaults.setObject_forKey_(ObjC.classes.NSData.alloc().initWithBase64EncodedString_options_(value, 0), key)
+  } else {
+    throw new Error(`Unable to update ${key} with ${value}, either type mismatch or unsupported type`)
+  }
+}
+
+export function remove(key: string) {
+  // no need to do existence check
+  singleton().removeObjectForKey_(key)
+}
+
+// @todo: shall we support value creation?
