7408529170e9d845618ff900ce511c2a7e57175e
cc <codecolorist@gmail.com>
2023-08-20 19:22:37 +0200

    remove unused module

--- agent/src/modules/device.ts
+++
@@ -1,9 +0,0 @@
-export function info() {
-  const keys = ['name', 'systemVersion', 'buildVersion', 'systemName', 'model', 'localizedModel']
-  const device = ObjC.classes.UIDevice.currentDevice()
-  const result: {[key: string]: string } = {}
-
-  for (const key of keys)
-    result[key] = device[key]() + ''
-  return result
-}
