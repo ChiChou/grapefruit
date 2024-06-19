b3b5a239b2d33eb33e93cf96c750422db52afcb7
cc <codecolorist@gmail.com>
2024-06-19 21:02:03 +0200

    chore: Update npm dependencies to latest versions update vite config to latest

--- guiv3/vite.config.ts
+++ guiv3/vite.config.ts
@@ -1,11 +1,10 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import pluginRewriteAll from 'vite-plugin-rewrite-all'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), pluginRewriteAll()],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
