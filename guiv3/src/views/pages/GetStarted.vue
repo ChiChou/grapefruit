22fce2b73987d0a300ebbfddeafddf69a1392c66
cc <codecolorist@gmail.com>
2024-06-20 02:39:22 +0200

    add more links to GetStarted

--- guiv3/src/views/pages/GetStarted.vue
+++ guiv3/src/views/pages/GetStarted.vue
@@ -1,9 +1,10 @@
 <template>
   <article class="get-started">
-    
-    <a target="_blank" 
-      href="https://mobile-security.gitbook.io/mobile-security-testing-guide/ios-testing-guide/0x06a-platform-overview">
-      OWASP MASTG iOS Platform Overview</a>
+    <ul>
+      <li><a target="_blank" href="https://mobile-security.gitbook.io/mobile-security-testing-guide/ios-testing-guide/0x06a-platform-overview">OWASP MASTG iOS Platform Overview</a></li>
+      <li><a target="_blank" href="https://book.hacktricks.xyz/mobile-pentesting/ios-pentesting-checklist">iOS Pentesting Checklist</a></li>
+      <li><a target="_blank" href="https://github.com/ivan-sincek/ios-penetration-testing-cheat-sheet">iOS Penetration Testing Cheat Sheet</a></li>
+    </ul>
   </article>
 </template>
 
