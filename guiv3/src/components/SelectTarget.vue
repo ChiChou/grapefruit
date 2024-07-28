29d8cfcfdd637f8f57ded027baabcaa688a8e1ea
cc <codecolorist@gmail.com>
2024-07-28 03:05:59 +0200

    bring back simulators

--- guiv3/src/components/SelectTarget.vue
+++ guiv3/src/components/SelectTarget.vue
@@ -39,7 +39,7 @@ const socket = io('/devices', { transports: ['websocket'] })
 onMounted(() => {
   socket.on('deviceChanged', refresh)
   refresh()
-  // reloadSimulators()
+  reloadSimulators()
 })
 
 onBeforeUnmount(() => socket.close())
@@ -98,9 +98,7 @@ function refresh() {
           <span v-if="devices.length === 0">No iPhone detected</span>
         </nav>
 
-        <!-- frida is currently broken for attaching Simulator apps -->
-        <!-- https://github.com/frida/frida/issues/2763 -->
-        <!-- <n-divider />
+        <n-divider />
         
         <p class="label">Simulators</p>
 
@@ -109,7 +107,7 @@ function refresh() {
             {{ sim.name }}</router-link>
 
           <span v-if="simulators.length === 0">No simulator running</span>
-        </nav> -->
+        </nav>
 
         <n-divider />
 
