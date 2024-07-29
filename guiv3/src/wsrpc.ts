d1b41a587bf28ba43f48b8b0fae19443ff482aff
cc <codecolorist@gmail.com>
2024-07-29 22:15:51 +0200

    refactor ready logic for wsrpc

--- guiv3/src/wsrpc.ts
+++ guiv3/src/wsrpc.ts
@@ -4,13 +4,9 @@
 // todo: rewrite this module
 
 import { Socket } from 'socket.io-client'
+import { RemoteRPC } from './rpc/registry';
 
-export type RPC = {
-  [key: string]: RPC;
-  (...args: any): any;
-}
-
-type WSEvent = 'ready' | 'destroyed' |
+export type WSEvent = 'ready' | 'destroyed' |
   'exception' | 'detached' | 'console' | 'crash' |
   'download' | 'delivery' | 'richconsole'
 
@@ -20,70 +16,56 @@ interface RpcResponse {
   error: string;
 }
 
-type Handler = (...args: any[]) => void;
-
 class Lazy {
-  chain: string[] = []
+  prefix: string[] = []
   ready = false
-  private _pending: Handler[] = []
 
   constructor(public socket: Socket) {
-    socket.on('ready', () => {
-      this.ready = true
-      this._pending.forEach(f => f())
-      this._pending = []
-    })
-  }
-
-  ensureReady(): Promise<boolean> {
-    if (this.ready) return Promise.resolve(true)
-    return new Promise((resolve) =>
-      this._pending.push(() =>
-        resolve(true)
-      ))
+    socket.once('ready', () => { this.ready = true })
   }
 
   push(name: string): Lazy {
-    this.chain.push(name)
+    this.prefix.push(name)
     return this
   }
 
   apply(argArray: any): Promise<any> {
     let name: string, args: any
-    if (this.chain.length) {
-      name = this.chain.join('.')
+    if (this.prefix.length) {
+      name = this.prefix.join('.')
       args = argArray
     } else {
       [name, ...args] = argArray
     }
-    this.chain = []
-    return this.ensureReady().then(() => {
-      return new Promise((resolve, reject) => {
-        let ok = false
+    this.prefix = []
+    const call = new Promise((resolve, reject) => {
+      const execute = () => {
         this.socket.emit('rpc', name, args, (response: RpcResponse) => {
           if (response.status === 'ok') {
-            ok = true
             resolve(response.data)
           } else {
             reject(new Error(response.error))
           }
         })
+      }
 
-        setTimeout(() => {
-          if (!ok) {
-            reject(new Error('Request timed out'))
-          }
-        }, 10000)
-      })
+      if (this.ready) {
+        execute()
+      } else {
+        this.socket.once('ready', execute)
+      }
     })
+
+    const timeout = new Promise((_, reject) => setTimeout(() => reject, 10000))
+    return Promise.race([call, timeout])
   }
 }
 
-export function useRPC(socket: Socket): RPC {
+export function useRPC(socket: Socket): RemoteRPC {
   const lazy = new Lazy(socket)
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   const p = new Proxy(() => { }, {
-    get(target: any, name: string): RPC {
+    get(target: any, name: string): any {
       lazy.push(name)
       return p
     },
