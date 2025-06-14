123e0fae9dc427c80513799c2ba5400ac530ab81
cc <codecolorist@gmail.com>
2025-06-14 16:59:08 +0200

    rename script

---
+++ bin/igf.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env node --experimental-strip-types --env-file=.env
+
+import type { AddressInfo } from "node:net";
+
+import env from "../server/lib/env.ts";
+import { realtime, server } from "../server/index.ts";
+
+server.listen(env.port, env.host, () => {
+  const addr = server.address() as AddressInfo;
+  console.log(`Server is running on http://${addr.address}:${addr.port}`);
+  console.log("environment:", env);
+});
+
+process.on("exit", () => realtime.close());
