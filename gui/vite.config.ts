7e9602fc014ce9c98829411d99fff79accc70046
cc <codecolorist@gmail.com>
2025-11-12 00:08:42 +0100

    set import alias for schema

--- gui/vite.config.ts
+++ gui/vite.config.ts
@@ -27,6 +27,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@shared/schema": path.resolve(__dirname, "..", "shared", "schema.d.ts"),
     },
   },
 });
