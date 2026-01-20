b2600a187baf4c43efe3ff0cf75d067af8b6df08
cc <codecolorist@gmail.com>
2026-01-20 15:54:53 +0800

    fix socket instance bug

--- gui/src/components/SessionProvider.tsx
+++ gui/src/components/SessionProvider.tsx
@@ -1,4 +1,4 @@
-import { type ReactNode, useMemo, useEffect, useState, useRef } from "react";
+import { type ReactNode, useMemo, useEffect, useState } from "react";
 import { Navigate, useParams } from "react-router";
 import { io, Socket } from "socket.io-client";
 
@@ -19,11 +19,6 @@ function SessionProvider({ children }: { children: ReactNode }) {
   const [status, setStatus] = useState<StatusType>(Status.Disconnected);
   const [pid, setPid] = useState<number | undefined>();
 
-  const socketRef = useRef<Socket<
-    SessionClientEvents,
-    SessionServerEvents
-  > | null>(null);
-
   const { socket, api } = useMemo(() => {
     if (!device || !bundle) {
       console.warn("Device or bundle ID missing from URL.");
@@ -31,20 +26,14 @@ function SessionProvider({ children }: { children: ReactNode }) {
       return { socket: null, api: null };
     }
 
-    const newSocket: Socket<SessionClientEvents, SessionServerEvents> = io(
+    const socket: Socket<SessionClientEvents, SessionServerEvents> = io(
       "/session",
       {
         query: { device, bundle },
       },
     );
 
-    if (socketRef.current) {
-      console.debug("disconnect websocket");
-      socketRef.current.disconnect();
-    }
-    socketRef.current = newSocket;
-
-    newSocket
+    socket
       .on("ready", (newPid: number) => {
         console.log("socket.io ready");
         setStatus(Status.Ready);
@@ -66,12 +55,12 @@ function SessionProvider({ children }: { children: ReactNode }) {
         setPid(undefined);
       });
 
-    const newApi = createAPI(newSocket);
+    const api = createAPI(socket);
     setStatus(Status.Connecting);
 
     return {
-      socket: newSocket,
-      api: newApi,
+      socket,
+      api,
     };
   }, [device, bundle]);
 
