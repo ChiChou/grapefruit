216bb1c6765c3b06a8b422c229331cadcb48319d
cc <codecolorist@gmail.com>
2026-01-22 22:44:50 +0800

    support detaching objc hooks

--- agent/src/fruity/hooks/objc.ts
+++ agent/src/fruity/hooks/objc.ts
@@ -10,8 +10,19 @@ export interface ObjCMessage extends BaseMessage {
   ret: string;
 }
 
+const hooked = new Map<string, Map<string, InvocationListener>>();
+
 export function swizzle(cls: string, sel: string) {
   if (!ObjC.available) throw new Error("Objective-C runtime is not available");
+
+  {
+    const methods = hooked.get(cls);
+    if (!methods) {
+      hooked.set(cls, new Map());
+    } else if (methods.has(sel)) {
+      return;
+    }
+  }
 
   const method = ObjC.classes[cls][sel] as ObjC.ObjectMethod | undefined;
   if (!method) throw new Error(`Method ${cls} ${sel} not found`);
@@ -21,7 +32,7 @@ export function swizzle(cls: string, sel: string) {
   const format = (t: string, v: NativePointer) =>
     t === "NSObject*" ? new ObjC.Object(v).toString() : `${v}`;
 
-  Interceptor.attach(method.implementation, {
+  const listener = Interceptor.attach(method.implementation, {
     onEnter(args) {
       const formatted = parsed.args.map((t, i) => format(t, args[i + 2]));
 
@@ -49,4 +60,21 @@ export function swizzle(cls: string, sel: string) {
       });
     },
   });
+
+  hooked.get(cls)!.set(sel, listener);
+}
+
+export function unswizzle(cls: string, sel: string) {
+  const methods = hooked.get(cls);
+  if (!methods) return;
+
+  const listener = methods.get(sel);
+  if (!listener) return;
+
+  listener.detach();
+  methods.delete(sel);
+
+  if (methods.size === 0) {
+    hooked.delete(cls);
+  }
 }
