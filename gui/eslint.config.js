c02f9ba914d1759fba3e560084be7539d905d380
cc <codecolorist@gmail.com>
2026-01-25 00:07:18 +0800

    do not lint shadcn files

--- gui/eslint.config.js
+++ gui/eslint.config.js
@@ -6,7 +6,11 @@ import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(["dist"]),
+  globalIgnores([
+    "dist",
+    "./src/components/ui/**/*",
+    "./src/components/theme-provider.tsx",
+  ]),
   {
     files: ["**/*.{ts,tsx}"],
     extends: [
