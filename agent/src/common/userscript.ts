b3a6dc4c9722bc11963711cb86a2e294ce555703
cc <codecolorist@gmail.com>
2026-02-05 23:02:27 +0100

    add user script runner todo: server side

--- agent/src/common/userscript.ts
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function evaluate(source: string, name?: string): any {
-  // todo: how do we bridge ObjC?
-  return Script.evaluate(name || "userscript", source);
-}
