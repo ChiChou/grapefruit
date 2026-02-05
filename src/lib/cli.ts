42cb63e80ade46fede3c5c0a495c43f49decbb6d
cc <codecolorist@gmail.com>
2026-02-05 10:56:26 +0100

    add cli options bugfix: server is always listening on 0.0.0.0 no matter the host option

---
+++ src/lib/cli.ts
@@ -0,0 +1,12 @@
+import { ParseArgsConfig } from "node:util";
+
+export const schema: ParseArgsConfig = {
+  args: process.argv.slice(2),
+  options: {
+    frida: { type: "string" },
+    host: { type: "string" },
+    port: { type: "string" },
+    help: { type: "boolean", short: "h" },
+  },
+  strict: false,
+};
