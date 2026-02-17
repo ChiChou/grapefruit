e13419bdfd381b020b704fa43524a490844ab8c6
cc <codecolorist@gmail.com>
2026-02-17 02:11:04 +0100

    truncate long ArrayBuffer content

--- agent/src/common/encode-arraybuffer.ts
+++ agent/src/common/encode-arraybuffer.ts
@@ -2,12 +2,17 @@ import { fromByteArray } from "base64-js";
 
 declare global {
   interface ArrayBuffer {
-    toJSON(): { type: "ArrayBuffer"; base64: string };
+    toJSON(): { type: "ArrayBuffer"; base64: string; length: number };
   }
 }
 
 // workaround: send() cannot serialize ArrayBuffer
+// this function is just to show more friendly representation of an ArrayBuffer
+// the agent should always use send({}, data) for blobs
 ArrayBuffer.prototype.toJSON = function () {
+  const limit = 32;
   const u8 = new Uint8Array(this);
-  return { type: "ArrayBuffer", base64: fromByteArray(u8) };
+  let base64 = fromByteArray(u8);
+  if (base64.length > limit) base64 = base64.slice(0, limit) + "...";
+  return { type: "ArrayBuffer", base64, length: this.byteLength };
 };
