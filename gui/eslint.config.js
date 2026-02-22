3a8ff92a2679f2ab4c2401218d1cc1cad458a897
cc <codecolorist@gmail.com>
2026-02-22 19:49:15 +0100

    fix eslint reporting errors from shadcn component

--- gui/eslint.config.js
+++ gui/eslint.config.js
@@ -23,17 +23,8 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-  },
-  {
-    files: ["src/components/ui/**/*.{ts,tsx}"],
     rules: {
       "react-refresh/only-export-components": "off",
-      "react-hooks/exhaustive-deps": [
-        "warn",
-        {
-          additionalHooks: "useTranslation|t",
-        },
-      ],
     },
   },
 ]);
