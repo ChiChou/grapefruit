518e007b156ea0ee91a48ad5c92a4e027b4599d7
cc <codecolorist@gmail.com>
2026-03-23 23:52:57 +0100

    fix critical UAF

--- agent/src/fruity/lib/weak.ts
+++ agent/src/fruity/lib/weak.ts
@@ -1,7 +1,16 @@
 import ObjC from "frida-objc-bridge";
+import { StringLike } from "../typings.js";
+
+interface NSMapTable extends ObjC.Object {
+  retain(): NSMapTable;
+  setObject_forKey_(object: ObjC.Object, key: StringLike): void;
+  objectForKey_(key: StringLike): ObjC.Object | null;
+  release(): void;
+}
 
 export class RefTracker {
-  private table = ObjC.classes.NSMapTable.strongToWeakObjectsMapTable();
+  private table: NSMapTable =
+    ObjC.classes.NSMapTable.strongToWeakObjectsMapTable().retain();
 
   put(handle: string, instance: ObjC.Object) {
     this.table.setObject_forKey_(instance, handle);
@@ -16,6 +25,10 @@ export class RefTracker {
     if (!obj) throw new Error(`(${handle}) has been deallocated`);
     return obj as ObjC.Object;
   }
+
+  release() {
+    this.table.release();
+  }
 }
 
 let singleton: RefTracker | null = null;
@@ -24,3 +37,7 @@ export function getTracker(): RefTracker {
   if (!singleton) singleton = new RefTracker();
   return singleton;
 }
+
+Script.bindWeak(globalThis, () => {
+  singleton?.release();
+});
