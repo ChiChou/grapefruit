d3b98564fb3bc2c4f839657623439a2a2af6bb86
cc <codecolorist@gmail.com>
2026-03-24 23:54:41 +0100

    fix github workflows

--- scripts/prepare.ts
+++ scripts/prepare.ts
@@ -6,3 +6,4 @@ await mkdir("./agent/dist", { recursive: true });
 execSync(`bun i`, { cwd: "./agent", stdio: "inherit" });
 execSync(`bun i`, { cwd: "./gui", stdio: "inherit" });
 execSync(`bun gui/scripts/fetch-r2.ts`, { stdio: "inherit" });
+execSync("make", { cwd: "./externals/radare/r2hermes.wasm", stdio: "inherit" });
