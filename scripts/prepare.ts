b6241658e6eb8e5f02c16c7cedf6f56f499920b6
cc <codecolorist@gmail.com>
2026-03-26 20:54:34 +0100

    fix: update prepare script to use correct r2 wasm fetch path

--- scripts/prepare.ts
+++ scripts/prepare.ts
@@ -5,4 +5,4 @@ await mkdir("./agent/dist", { recursive: true });
 
 execSync(`bun i`, { cwd: "./agent", stdio: "inherit" });
 execSync(`bun i`, { cwd: "./gui", stdio: "inherit" });
-execSync(`bun gui/scripts/fetch-r2.ts`, { stdio: "inherit" });
+execSync(`bun scripts/fetch-r2-wasm.ts`, { stdio: "inherit" });
